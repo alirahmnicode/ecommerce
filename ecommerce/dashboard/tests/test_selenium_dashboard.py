60dbf02d8fae12d81ac4fc7efa052e8872b9c272
alirahmanicode <alirahmanicode@gmail.com>
2023-01-05 20:05:07 +0330

    login test

--- ecommerce/dashboard/tests/test_selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -1,2 +1,21 @@
-def test_example():
-    assert "a" == "an"
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+@pytest.mark.selenium
+def test_dashboard_admin_login(live_server, db_fixture_setup, chrome_browser_instance):
+
+    browser = chrome_browser_instance
+
+    browser.get(("%s%s" % (live_server.url, "/admin/login")))
+
+    username = browser.find_element(By.NAME, "username")
+    password = browser.find_element(By.NAME, "password")
+    submit = browser.find_element(By.XPATH, "//input[@value='Log in']")
+
+    username.send_keys("admin")
+    password.send_keys("admin")
+    submit.send_keys(Keys.RETURN)
+
+    assert "Site administration" in browser.page_source
