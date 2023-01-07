be9ea7f4ce51e408bd8d995b1ea85354410e1e68
alirahmanicode <alirahmanicode@gmail.com>
2023-01-07 17:53:54 +0330

    creat category product model

--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils.translation import gettext as _
+from mptt.models import MPTTModel, TreeForeignKey, TreeManyToManyField
 
-# Create your models here.
+
+class Category(MPTTModel):
+    """
+    Inventory Category table implimented with MPTT
+    """
+
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        unique=False,
+        blank=False,
+        verbose_name=_("category name"),
+        help_text=_("forma: required, max-100"),
+    )
+    slug = models.SlugField(
+        max_length=150,
+        null=False,
+        unique=False,
+        blank=False,
+        verbose_name=_("category safe URL"),
+        help_text=_("forma: required, letters, numbers, underscore, or hyphens"),
+    )
+    is_active = models.BooleanField(default=True)
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.PROTECT,
+        related_name="children",
+        null=True,
+        blank=True,
+        unique=False,
+        verbose_name=_("parent of category"),
+        help_text=_("forma: not required"),
+    )
+
+    class MpttMeta:
+        order_insertion_by = ["nae"]
+
+    class Meta:
+        verbose_name = _("product category")
+        verbose_name_plural = _("product categories")
+
+    def __str__(self) -> str:
+        return self.name
