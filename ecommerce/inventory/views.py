1c68a2e7b96dbc17db8b46a1fd5ec48d71f33fee
alirahmanicode <alirahmanicode@gmail.com>
2023-02-09 17:37:19 +0330

    create index view

--- ecommerce/inventory/views.py
+++ ecommerce/inventory/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from . import models
+
+
 # Create your views here.
+def index(request):
+    categories = models.Category.objects.all()[:10]
+    brands = models.Brand.objects.all()[:4]
+    shoes = models.Product.objects.filter(category__name="comfort")[:4]
+    data = {"categories": categories, "brands": brands}
+    return render(request, "inventory/index.html", {"data": data})
