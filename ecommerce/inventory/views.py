c5f660538d857aeb73347760520d590a85993daa
alirahmanicode <alirahmanicode@gmail.com>
2023-02-10 16:24:02 +0330

    add new products

--- ecommerce/inventory/views.py
+++ ecommerce/inventory/views.py
@@ -7,6 +7,12 @@ from . import models
 def index(request):
     categories = models.Category.objects.all()[:10]
     brands = models.Brand.objects.all()[:4]
-    shoes = models.Product.objects.filter(category__name="comfort")[:4]
-    data = {"categories": categories, "brands": brands}
+    new_products = models.ProductInventory.objects.all()
+    discount_products = list(new_products)[:4]
+    data = {
+        "categories": categories,
+        "brands": brands,
+        "new_products": new_products,
+        "discount_products": discount_products,
+    }
     return render(request, "inventory/index.html", {"data": data})
