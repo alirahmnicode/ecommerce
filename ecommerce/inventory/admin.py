a97bd073f0dacf3fecabfcf124357c765e6ff9d3
alirahmanicode <alirahmanicode@gmail.com>
2023-02-10 16:24:28 +0330

    add models to admin

--- ecommerce/inventory/admin.py
+++ ecommerce/inventory/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from ecommerce.inventory.models import Category
+from ecommerce.inventory.models import Category, Product, ProductInventory
 
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductInventory)
