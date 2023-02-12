7fdc350850df0ec01c56736706a5301d8d092522
alirahmanicode <alirahmanicode@gmail.com>
2023-02-12 20:59:13 +0330

    compelete product detail view

--- ecommerce/inventory/urls.py
+++ ecommerce/inventory/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = "inventory"
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("product/detail/<slug:slug>/", views.product_detail, name="detail"),
+]
--- ecommerce/inventory/views.py
+++ ecommerce/inventory/views.py
@@ -1,3 +1,5 @@
+import pprint
+
 from django.shortcuts import render
 
 from . import models
@@ -16,3 +18,43 @@ def index(request):
         "discount_products": discount_products,
     }
     return render(request, "inventory/index.html", {"data": data})
+
+
+def product_detail(request, slug):
+
+    from django.contrib.postgres.aggregates import ArrayAgg
+
+    product = (
+        models.ProductInventory.objects.filter(product__slug=slug)
+        .filter(is_default=True)
+        .values(
+            "id",
+            "sku",
+            "product__name",
+            "product__description",
+            "store_price",
+            "product_inventory__units",
+            "brand",
+        )
+        .annotate(field_a=ArrayAgg("attribute_values__attribute_value"))
+        .get()
+    )
+    y = (
+        models.ProductInventory.objects.filter(product__slug=slug)
+        .distinct()
+        .values(
+            "attribute_values__product_attribute__name",
+            "attribute_values__attribute_value",
+        )
+    )
+
+    z = (
+        models.ProductTypeAttribute.objects.filter(
+            product_type__product_type__product__slug=slug
+        )
+        .distinct()
+        .values("product_attribute__name")
+    )
+    pprint.pprint(product["field_a"])
+    context = {"product": product, "filter": y, "z": z}
+    return render(request, "inventory/product_detail.html", context)
