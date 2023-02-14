16ab721d04d01df8069f15b30c00a49dc00c288f
alirahmanicode <alirahmanicode@gmail.com>
2023-02-14 19:47:29 +0330

    change class param

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -2,9 +2,8 @@ from ecommerce.inventory.models import ProductInventory
 
 
 class Cart:
-    def __init__(self, request, product_id: int) -> None:
+    def __init__(self, request) -> None:
         self.session = request.session
-        self.product_id = product_id
         cart = self.session.get("cart")
         if not cart:
             cart = self.session["cart"] = {}
@@ -15,13 +14,18 @@ class Cart:
             products = self.cart["products"] = {}
         self.products = products
 
-    def add(self):
+        print(self.cart)
+
+    def add(self, product_id):
         # add new product to products
-        product_id = str(self.product_id)
+        product_id = str(product_id)
         if product_id in self.products.keys():
             self.products[product_id]["quantity"] += 1
         else:
-            self.products[product_id] = {"quantity": 1, "product": self.get_product()}
+            self.products[product_id] = {
+                "quantity": 1,
+                "product": self.get_product(product_id),
+            }
         # update total price
         self.cart["total_price"] = self.get_total_price()
         self.save()
@@ -36,8 +40,8 @@ class Cart:
         )
         return total_price
 
-    def get_product(self):
-        product = ProductInventory.objects.filter(id=self.product_id).values(
+    def get_product(self, product_id):
+        product = ProductInventory.objects.filter(id=product_id).values(
             "product__name", "store_price"
         )
         product_price = list(product)[0]["store_price"]
