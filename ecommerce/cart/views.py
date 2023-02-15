f77c42b9c96a991740bb3ce88c918dfad9190fe9
alirahmanicode <alirahmanicode@gmail.com>
2023-02-15 18:32:10 +0330

    cart view

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,6 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def cart(request):
-    pass
+    cart = request.session.get("cart")
+    return render(request, "cart/cart.html", {"cart": cart})
