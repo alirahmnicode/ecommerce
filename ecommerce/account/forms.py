717699d2daad70a06961ef2523435096a00ecd28
alirahmanicode <alirahmanicode@gmail.com>
2023-03-06 20:09:32 +0330

    add class to form fields

--- ecommerce/account/forms.py
+++ ecommerce/account/forms.py
@@ -1,15 +1,28 @@
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.forms import ModelForm
 
 from .models import CustomUser
 
 
-class CustomUserCreationForm(UserCreationForm):
+class BaseCustomUserForm(ModelForm):
+    """
+    this class created for add form-control class to all
+    fields.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(BaseCustomUserForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs["class"] = "form-control"
+
+
+class CustomUserCreationForm(BaseCustomUserForm, UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("phone",)
 
 
-class CustomUserChangeForm(UserChangeForm):
+class CustomUserChangeForm(BaseCustomUserForm, UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("phone",)
