f1b6956d027c067ddbb4c2cb39a19ea33166e162
alirahmanicode <alirahmanicode@gmail.com>
2023-03-06 19:03:55 +0330

    create costum user model

--- ecommerce/account/models.py
+++ ecommerce/account/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    phone = models.TextField(_("phone number"), unique=True, max_length=11)
+
+    USERNAME_FIELD = "phone"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.phone
--- ecommerce/account/tests.py
+++ ecommerce/account/tests.py
@@ -1,3 +1,40 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(phone="09213141774", password="foo")
+        self.assertEqual(user.phone, "09213141774")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        try:
+            self.assertIsNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(phone="")
+        with self.assertRaises(ValueError):
+            User.objects.create_user(phone="", password="foo")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(phone="09213141774", password="foo")
+        self.assertEqual(admin_user.phone, "09213141774")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            # username is None for the AbstractUser option
+            # username does not exist for the AbstractBaseUser option
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                phone="09213141774", password="foo", is_superuser=False
+            )
