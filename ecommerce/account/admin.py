f1c434d3bf85efca51deb6545f3441ae7a70cf3e
alirahmanicode <alirahmanicode@gmail.com>
2023-03-06 19:19:46 +0330

    add to admin pannel

--- ecommerce/account/admin.py
+++ ecommerce/account/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ("phone", "is_staff", "is_active")
+    list_filter = ("phone", "is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("phone", "password")}),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "phone",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+    ordering = ("email",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
