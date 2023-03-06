82dfdc30fe51376fafa60c170145e2c2d1c95c59
alirahmanicode <alirahmanicode@gmail.com>
2023-03-06 20:10:50 +0330

    Create sign up view

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        print(form)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "ثبت نام با موفقیت انجام شد.")
+            return redirect("inventory:index")
+        messages.error(request, "هنگام ثبت نام خطایی رخ داد.")
+    form = CustomUserCreationForm()
+    return render(request, "account/signup.html", {"form": form})
+
+
+def user_login(request):
+    pass
+
+
+def user_logout(request):
+    pass
