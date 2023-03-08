539359ee5aca40446b81fac37b8ac1ab7b7d2f99
alirahmanicode <alirahmanicode@gmail.com>
2023-03-08 21:52:09 +0330

    edit model

--- ecommerce/account/models.py
+++ ecommerce/account/models.py
@@ -16,3 +16,16 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.phone
+
+
+class Profile(models.Model):
+    user = models.OneToOneField("CustomUser", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
+    address = models.TextField(blank=False, help_text=_("street 11, block 4"))
+    postal_code = models.TextField(max_length=20, help_text=_("312342131232"))
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.user.phone
