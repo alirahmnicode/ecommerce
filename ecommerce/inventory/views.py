6b454c82aadd62b994c96ed158af0724ce6953f4
alirahmanicode <alirahmanicode@gmail.com>
2023-03-18 16:38:57 +0330

    update template

--- ecommerce/inventory/views.py
+++ ecommerce/inventory/views.py
@@ -9,13 +9,11 @@ from .sort import Sort
 
 # Create your views here.
 def index(request):
-    brands = models.Brand.objects.all()[:4]
+    brands = models.Brand.objects.all()
     new_products = models.ProductInventory.objects.all()
-    discount_products = list(new_products)[:4]
     data = {
         "brands": brands,
         "new_products": new_products,
-        "discount_products": discount_products,
     }
     return render(request, "inventory/index.html", {"data": data})
 
