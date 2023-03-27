14c38cdb551ae7facdbccedd09c9cd0834b0d385
alirahmanicode <alirahmanicode@gmail.com>
2023-03-27 17:11:25 +0430

    change fixtures

--- ecommerce/inventory/models.py
+++ ecommerce/inventory/models.py
@@ -55,14 +55,6 @@ class Product(models.Model):
     Product details table
     """
 
-    web_id = models.CharField(
-        max_length=50,
-        unique=True,
-        null=False,
-        blank=False,
-        verbose_name=_("product website ID"),
-        help_text=_("format: required, unique"),
-    )
     slug = models.SlugField(
         max_length=255,
         unique=False,
@@ -129,75 +121,6 @@ class Brand(models.Model):
         return self.name
 
 
-class ProductAttribute(models.Model):
-    """
-    Product attribute table
-    """
-
-    name = models.CharField(
-        max_length=255,
-        unique=True,
-        null=False,
-        blank=False,
-        verbose_name=_("product attribute name"),
-        help_text=_("format: required, unique, max-255"),
-    )
-    description = models.TextField(
-        unique=False,
-        null=False,
-        blank=False,
-        verbose_name=_("product attribute description"),
-        help_text=_("format: required"),
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class ProductType(models.Model):
-    """
-    Product type table
-    """
-
-    name = models.CharField(
-        max_length=255,
-        unique=True,
-        null=False,
-        blank=False,
-        verbose_name=_("type of product"),
-        help_text=_("format: required, unique, max-255"),
-    )
-
-    product_type_attributes = models.ManyToManyField(
-        ProductAttribute,
-        related_name="product_type_attributes",
-        through="ProductTypeAttribute",
-    )
-
-    def __str__(self):
-        return self.name
-
-
-class ProductAttributeValue(models.Model):
-    """
-    Product attribute value table
-    """
-
-    product_attribute = models.ForeignKey(
-        ProductAttribute,
-        related_name="product_attribute",
-        on_delete=models.PROTECT,
-    )
-    attribute_value = models.CharField(
-        max_length=255,
-        unique=False,
-        null=False,
-        blank=False,
-        verbose_name=_("attribute value"),
-        help_text=_("format: required, max-255"),
-    )
-
-
 class ProductInventory(models.Model):
     """
     Product inventory table
@@ -219,18 +142,10 @@ class ProductInventory(models.Model):
         verbose_name=_("universal product code"),
         help_text=_("format: required, unique, max-12"),
     )
-    product_type = models.ForeignKey(
-        ProductType, related_name="product_type", on_delete=models.PROTECT
-    )
     product = models.ForeignKey(
         Product, related_name="product", on_delete=models.PROTECT
     )
     brand = models.ForeignKey(Brand, related_name="brand", on_delete=models.PROTECT)
-    attribute_values = models.ManyToManyField(
-        ProductAttributeValue,
-        related_name="product_attribute_values",
-        through="ProductAttributeValues",
-    )
     is_active = models.BooleanField(
         default=True,
         verbose_name=_("product visibility"),
@@ -383,43 +298,3 @@ class Stock(models.Model):
         verbose_name=_("units sold to date"),
         help_text=_("format: required, default-0"),
     )
-
-
-class ProductAttributeValues(models.Model):
-    """
-    Product attribute values link table
-    """
-
-    attributevalues = models.ForeignKey(
-        "ProductAttributeValue",
-        related_name="attributevaluess",
-        on_delete=models.PROTECT,
-    )
-    productinventory = models.ForeignKey(
-        ProductInventory,
-        related_name="productattributevaluess",
-        on_delete=models.PROTECT,
-    )
-
-    class Meta:
-        unique_together = (("attributevalues", "productinventory"),)
-
-
-class ProductTypeAttribute(models.Model):
-    """
-    Product type attributes link table
-    """
-
-    product_attribute = models.ForeignKey(
-        ProductAttribute,
-        related_name="productattribute",
-        on_delete=models.PROTECT,
-    )
-    product_type = models.ForeignKey(
-        ProductType,
-        related_name="producttype",
-        on_delete=models.PROTECT,
-    )
-
-    class Meta:
-        unique_together = (("product_attribute", "product_type"),)
