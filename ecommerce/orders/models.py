a9e028f083d38c58a4f979fd5fa1eeb4c14f9a2d
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-04-04 21:19:23 +0430

    create order model

--- ecommerce/orders/models.py
+++ ecommerce/orders/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+
+    first_name = models.CharField(_("First Name:"), max_length=225)
+    last_name = models.CharField(_("Last Name:"), max_length=225)
+    address = models.CharField(_("Address"), max_length=1000)
+
+    order_notes = models.CharField(_("Order Notes"), max_length=700, blank=True)
+
+    datetime_created = models.DateTimeField(_("Created"), auto_now_add=True)
+    datetime_modified = models.DateTimeField(_("Modified"), auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Order {self.id}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(
+        "inventory.ProductInventory",
+        on_delete=models.CASCADE,
+        related_name="order_items",
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"OrderItem {self.id}: {self.product} x {self.quantity} (price:{self.price})"
