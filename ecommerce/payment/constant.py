8337ce54b828517abb7a3fd389821c803001a468
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-04-07 19:30:02 +0430

    constants

---
+++ ecommerce/payment/constant.py
@@ -0,0 +1,4 @@
+CREATE_PAYMENT = "https://gateway.zibal.ir/v1/request"
+PAYMENT_URL = "https://gateway.zibal.ir/start/{trackId}"
+CALLBACK_URL = "http://127.0.0.1:8000/payment/result/"
+VEIYFY_URL = "https://gateway.zibal.ir/v1/inquiry"
