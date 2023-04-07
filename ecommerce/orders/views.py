f72cd471b4e2291cff9781bb8a12448b18670353
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-04-07 19:28:11 +0430

    update view

--- ecommerce/orders/views.py
+++ ecommerce/orders/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from ecommerce.cart.cart import Cart
 
 from .forms import OrderForm
+from .models import OrderItem
 
 
 def order_create_view(request):
+    cart = Cart(request)
     if request.method == "POST":
-        pass
+        order_form = OrderForm(request.POST)
+        if order_form.is_valid():
+            order_obj = order_form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+
+            for item in cart:
+                product = item["product_obj"]
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=product,
+                    quantity=item["quantity"],
+                    price=product.store_price,
+                )
+
+            cart.clear()
+
+            request.session["order_id"] = order_obj.id
+
+            return redirect("payment:process")
+
     form = OrderForm()
-    return render(request, "orders/create.html", {"form": form})
+    return render(request, "orders/create.html", {"form": form, "cart": cart})
