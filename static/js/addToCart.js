2d5036207fe735b836e4890f3142f653dcf3fd22
ali rahmani <70445184+alirahmnicode@users.noreply.github.com>
2023-04-09 22:48:33 +0430

    collect static

--- static/js/addToCart.js
+++ static/js/addToCart.js
@@ -1,12 +1,11 @@
 const addBtn = $('.add-to-cart')
 const url_origin = window.location.origin
-const action = "add"
 
 
 addBtn.click(function () {
     const product_id = this.dataset.product_id
     $.ajax({
-        url: `${url_origin}/cart/quantity/${product_id}/${action}/`,
+        url: `${url_origin}/cart/add/${product_id}/1/`,
         type: 'GET',
         success: function (data) {
             updateCartCounter(data)
@@ -16,6 +15,13 @@ addBtn.click(function () {
 })
 
 
-function updateCartCounter(cart) {
-    $('#header-qty').text(cart.cart.quantity)
+function updateCartCounter(data) {
+    var cart = data.cart
+    var quantity = 0
+
+    for (const i in cart) {
+        quantity += cart[i].quantity
+    }
+
+    $('#header-qty').text(quantity)
 }
